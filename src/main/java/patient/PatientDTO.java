fb22ac32bd072ad7024559059aa2efbfe12daf0b
Bugs Bunny <Bugs@email.com>
2024-12-15 21:28:46 +0100

    gha sber akhay anass osf

---
+++ src/main/java/patient/PatientDTO.java
@@ -0,0 +1,66 @@
+package patient;
+
+import javax.persistence.Embedded;
+
+
+import metier.entities.Localisation;
+
+public class PatientDTO {
+    private String nom;
+    private String prenom;
+    public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	private String email;
+    private String telephone;
+    private String password;
+
+    @Embedded
+    private Localisation localisation;
+
+    // Getters et setters
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Localisation getLocalisation() {
+        return localisation;
+    }
+
+    public void setLocalisation(Localisation localisation) {
+        this.localisation = localisation;
+    }
+}
